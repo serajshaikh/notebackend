c639bb4cfa9730413cb261a9e0b48971900f5470
Seraj <serazshaikh07@gmail.com>
2025-06-08 18:23:21 +0530

    update

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
-
 const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://serajsnote.netlify.app/",
+  "https://serajsnote.netlify.app",
   "https://apunp.netlify.app"
 ];
 
-app.use(cors({
+const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
@@ -25,20 +24,20 @@ app.use(cors({
   credentials: true,
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-}));
+};
 
-app.options("*", cors()); // Handles preflight
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (_, res) => {
-  try {
-    res.json({ message: 'Health check successful!', allowedOrigins });
-  } catch (err) {
-    res.status(500).json({ error: 'HealthCheck error' });
-  }
-})
+  res.json({ message: 'Health check successful!', allowedOrigins });
+});
+
 app.use('/api/auth', authRoutes);
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
